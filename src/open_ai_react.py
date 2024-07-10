0d8fc9bfb6e8de6122f3549b34f90bb0573294cc
crismolav <crismolav@gmail.com>
2024-07-10 14:47:05 +0200

    dotenv added + get_price action

--- src/open_ai_react.py
+++ src/open_ai_react.py
@@ -1,6 +1,8 @@
 import re
 from openai import OpenAI
+from dotenv import load_dotenv
 
+_ = load_dotenv()
 
 client = OpenAI()
 
@@ -17,13 +19,17 @@ calculate:
 e.g. calculate: 4 * 7 / 3
 Runs a calculation and returns the number - uses Python so be sure to use floating point syntax if necessary
 
+get_price:
+e.g. get_price: pencil
+Returns the price of an item when given the name of the item
+
 average_dog_weight:
 e.g. average_dog_weight: Collie
 returns average weight of a dog when given the breed
 
 Example session:
 
-Question: How much does a Bulldog weigh?
+Question: How much do 2 Bulldog weigh?
 Thought: I should look the dogs weight using average_dog_weight
 Action: average_dog_weight: Bulldog
 PAUSE
@@ -31,10 +37,16 @@ PAUSE
 You will be called again with this:
 
 Observation: A Bulldog weights 51 lbs
+Thought: I should calculate the weight of 2 bulldogs
+Action: calculate: 51 * 2
+PAUSE
+
+You will be called again with this:
+Observation: 102
 
 You then output:
 
-Answer: A bulldog weights 51 lbs
+Answer: 2 bulldogs weight 102 lbs
 """.strip()
 
 
@@ -53,9 +65,9 @@ class Agent:
 
     def execute(self):
         completion = client.chat.completions.create(
-                        model="gpt-3.5-turbo", #"gpt-4o",
-                        temperature=0,
-                        messages=self.messages)
+            model="gpt-3.5-turbo",  #"gpt-4o",
+            temperature=0,
+            messages=self.messages)
         return completion.choices[0].message.content
 
 
@@ -65,17 +77,27 @@ def calculate(what):
 
 def average_dog_weight(name):
     if name in "Scottish Terrier":
-        return("Scottish Terriers average 20 lbs")
+        return ("Scottish Terriers average 20 lbs")
     elif name in "Border Collie":
-        return("a Border Collies average weight is 37 lbs")
+        return ("a Border Collies average weight is 37 lbs")
     elif name in "Toy Poodle":
-        return("a toy poodles average weight is 7 lbs")
+        return ("a toy poodles average weight is 7 lbs")
     else:
-        return("An average dog weights 50 lbs")
+        return ("An average dog weights 50 lbs")
+
+
+def get_price(name):
+    if name in "pencil":
+        return "A pencil costs 1.0 Euros"
+    elif name in "notebook":
+        return "A notebook costs 2.5 Euros"
+    else:
+        return "An average item costs 3.5 Euros"
 
 
 known_actions = {
     "calculate": calculate,
+    "get_price": get_price,
     "average_dog_weight": average_dog_weight
 }
 
@@ -107,8 +129,9 @@ def query(question, max_turns=5):
             return
 
 
-question = """I have 2 dogs, a border collie and a scottish terrier. \
+question1 = """I have 2 dogs, a border collie and a scottish terrier. \
 What is their combined weight"""
-print(f"Question: {question}")
-query(question)
-
+question2 = """I have 3 items, 2 pencils and a notebook. \
+What is their combined cost?"""
+print(f"Question: {question2}")
+query(question2)
