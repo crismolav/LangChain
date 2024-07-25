fc971e7661b26e0fd7a91cceb871afb0e415d84f
crismolav <crismolav@gmail.com>
2024-07-25 20:46:04 +0200

    refactoring

--- src/lang_graph.py
+++ src/lang_graph.py
@@ -4,17 +4,59 @@ import operator
 from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.checkpoint.sqlite import SqliteSaver
 from dotenv import load_dotenv
+import uuid
+import time
+
+# Define a memory checkpoint for the agent with built-in database under the hood.
+memory = SqliteSaver.from_conn_string(":memory:")
 _ = load_dotenv()
-tool = TavilySearchResults(max_results=4)  # increased number of results
+tool = TavilySearchResults(max_results=2)
+
+
+def generate_unique_thread_id():
+    unique_part = uuid.uuid4().hex  # Generates a unique UUID
+    timestamp_part = str(int(time.time()))  # Current timestamp as an integer
+    return f"{timestamp_part}-{unique_part}"
 
 
 class AgentState(TypedDict):
+    """
+    Defines the state structure for an agent in the language graph.
+
+    Attributes:
+        messages (Annotated[list[AnyMessage], operator.add]): A list of messages that can be operated on.
+    """
     messages: Annotated[list[AnyMessage], operator.add]
 
 
 class Agent:
-    def __init__(self, model, tools, system=""):
+    """
+    Represents an agent that interacts with a state graph to process messages using OpenAI and custom tools.
+
+    Attributes:
+        system (str): An initial system message to prepend to all interactions.
+        graph (StateGraph): The compiled state graph for message processing.
+        tools (dict): A dictionary mapping tool names to their instances.
+        model (ChatOpenAI): The OpenAI model bound with tools for message processing.
+
+    Methods:
+        __init__(self, model, tools, system=""): Initializes the agent.
+        exists_action(self, state: AgentState): Checks if the last message in the state has any tool calls.
+        call_openai(self, state: AgentState): Processes messages through the OpenAI model.
+        take_action(self, state: AgentState): Invokes the appropriate tool based on the last message's tool calls.
+    """
+
+    def __init__(self, model, tools, checkpointer, system=""):
+        """
+        Initializes the agent with a model, tools, and an optional system message.
+
+        Parameters:
+            model (ChatOpenAI): The OpenAI model to use for processing messages.
+            tools (list): A list of tool instances available for the agent.
+            system (str, optional): An initial system message. Defaults to an empty string.
+        """
         self.system = system
         graph = StateGraph(AgentState)
         graph.add_node("llm", self.call_openai)
@@ -26,15 +68,31 @@ class Agent:
         )
         graph.add_edge("action", "llm")
         graph.set_entry_point("llm")
-        self.graph = graph.compile()
+        self.graph = graph.compile(checkpointer=checkpointer)
         self.tools = {t.name: t for t in tools}
         self.model = model.bind_tools(tools)
 
-    def exists_action(self, state: AgentState):
+    @staticmethod
+    def exists_action(state: AgentState):
+        """
+        Determines if the last message in the state contains any tool calls.
+
+        Parameters:
+            state (AgentState): The current state of the agent.
+
+        Returns:
+            bool: True if there are tool calls, False otherwise.
+        """
         result = state['messages'][-1]
         return len(result.tool_calls) > 0
 
     def call_openai(self, state: AgentState):
+        """
+        Processes messages through the OpenAI model.
+
+        Parameters:
+            state (AgentState): The current state of the agent.
+        """
         messages = state['messages']
         if self.system:
             messages = [SystemMessage(content=self.system)] + messages
@@ -42,38 +100,61 @@ class Agent:
         return {'messages': [message]}
 
     def take_action(self, state: AgentState):
-        tool_calls = state['messages'][-1].tool_calls
-        results = []
+        """
+        Processes tool calls from the last message in the state and invokes the corresponding tool.
+
+        This method iterates over each tool call found in the last message of the state. It checks if the tool name
+        exists within the agent's tools dictionary. If the tool name is not found, it logs a message indicating a bad
+        tool name and sets the result to a retry message. Otherwise, it invokes the tool with the provided arguments
+        and collects the results. Each result is wrapped in a ToolMessage object. After processing all tool calls,
+        it returns a dictionary containing the list of ToolMessage objects.
+
+        Parameters:
+            state (AgentState): The current state of the agent, containing messages among other information.
+
+        Returns:
+            dict: A dictionary with a key 'messages' containing a list of ToolMessage objects representing the results
+            of the invoked tools.
+        """
+        tool_calls = state['messages'][-1].tool_calls  # Extract tool calls from the last message
+        results = []  # Initialize an empty list to store results
         for t in tool_calls:
             print(f"Calling: {t}")
-            if not t['name'] in self.tools:      # check for bad tool name from LLM
+            if not t['name'] in self.tools:  # Check if the tool name exists in the agent's tools
                 print("\n ....bad tool name....")
-                result = "bad tool name, retry"  # instruct LLM to retry if bad
+                result = "bad tool name, retry"  # Set a retry message for the result
             else:
-                result = self.tools[t['name']].invoke(t['args'])
-            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
+                result = self.tools[t['name']].invoke(t['args'])  # Invoke the tool with arguments
+            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))  # Append the result
         print("Back to the model!")
-        return {'messages': results}
+        return {'messages': results}  # Return the results wrapped in a dictionary
 
 
 def main():
     prompt = """You are a smart research assistant. Use the search engine to look up information. \
     You are allowed to make multiple calls (either together or in sequence). \
     Only look up information when you are sure of what you want. \
+    If a user message is missing context or information you should ask clarifying questions. \
     If you need to look up some information before asking a follow up question, you are allowed to do that!
     """
-
-    model = ChatOpenAI(model="gpt-3.5-turbo")
-    abot = Agent(model, [tool], system=prompt)
-    # messages = [HumanMessage(content="What is the weather in sf?")]
-    # result = abot.graph.invoke({"messages": messages})
-    # print(result['messages'][-1].content)
-    query = input("Enter query: ")
-    messages = [HumanMessage(content=query)]
-    result = abot.graph.invoke({"messages": messages})
-    print(result['messages'][-1].content)
+    debug = True
+    chat_gpt_model = 'gpt-3.5-turbo'  # gpt-3.5-turbo, gpt-4o
+    model = ChatOpenAI(model=chat_gpt_model)
+    abot = Agent(model, [tool], system=prompt, checkpointer=memory)
+    print("Sample query: Who was the president of the United States in 1990 and who was their spouse back then")
+    thread = {"configurable": {"thread_id": generate_unique_thread_id()}}  # Define a thread ID for the conversation
+    while True:
+        query = input("Query: ")
+        messages = [HumanMessage(content=query)]  # Initialize the messages with the user query
+        # to allow for multiple conversations
+        if debug:
+            for event in abot.graph.stream({"messages": messages}, thread):
+                for v in event.values():
+                    print(v['messages'])
+        else:
+            result = abot.graph.invoke({"messages": messages}, thread)
+            print(result['messages'][-1].content)
 
 
 if __name__ == "__main__":
     main()
-
--- src/open_ai_react.py
+++ src/open_ai_react.py
@@ -1,4 +1,5 @@
 import re
+import ast
 from openai import OpenAI
 from dotenv import load_dotenv
 
@@ -11,6 +12,7 @@ You run in a loop of Thought, Action, PAUSE, Observation.
 At the end of the loop you output an Answer
 Use Thought to describe your thoughts about the question you have been asked.
 Use Action to run one of the actions available to you - then return PAUSE.
+If you are not sure of an action, you can ask the user for input using action ask_user.
 Observation will be the result of running those actions.
 
 Your available actions are:
@@ -26,6 +28,14 @@ Returns the price of an item when given the name of the item
 average_dog_weight:
 e.g. average_dog_weight: Collie
 returns average weight of a dog when given the breed
+
+sql_query:
+e.g. sql_query: ("brand", ["cocacola", "pepsi"], 0.5)
+returns a string "New Scenario created" string   - if you are missing any information call ask_user action
+
+ask_user: 
+e.g. ask_user: What percentage do you want to change the prices by?
+returns the user input.
 
 Example session:
 
@@ -47,6 +57,36 @@ Observation: 102
 You then output:
 
 Answer: 2 bulldogs weight 102 lbs
+
+Example session:
+
+Question: Increase all prices of brand cocacola and pepsi by 10%
+Thought: I should create a new sql query using sql_query
+Action: sql_query: ("brand", ["cocacola", "pepsi"], 0.1)
+PAUSE
+
+You will be called again with this:
+Observation: New Scenario created.
+
+You then output:
+
+Answer: New Scenario created.
+
+Example session:
+
+Question: Increase all prices of brand coca cola and pepsi.
+Thought: I'm missing the percentage to create a new sql query.
+Action: ask_user: What percentage do you want to change the prices by?
+PAUSE
+
+You will be called again with this:
+Observation: By -5 percent
+Thought: I should create a new sql query.
+Action: sql_query: ("brand", ["cocacola", "pepsi"], -0.05)
+
+You then output:
+
+Answer: New Scenario created.
 """.strip()
 
 
@@ -65,10 +105,14 @@ class Agent:
 
     def execute(self):
         completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",  #"gpt-4o",
+            model="gpt-3.5-turbo",  # "gpt-4o",
             temperature=0,
             messages=self.messages)
         return completion.choices[0].message.content
+
+
+def ask_user(user_question):
+    return input(f"{user_question}:")
 
 
 def calculate(what):
@@ -95,10 +139,17 @@ def get_price(name):
         return "An average item costs 3.5 Euros"
 
 
+def sql_query(column: str, values: list[str], change_price: float) -> str:
+    # makes sure to format all the values to the ones stored in the database
+    return "New Scenario created"
+
+
 known_actions = {
     "calculate": calculate,
     "get_price": get_price,
-    "average_dog_weight": average_dog_weight
+    "average_dog_weight": average_dog_weight,
+    "sql_query": sql_query,
+    "ask_user": ask_user
 }
 
 
@@ -119,13 +170,19 @@ def query(question, max_turns=5):
         if actions:
             # There is an action to run
             action, action_input = actions[0].groups()
+
             if action not in known_actions:
                 raise Exception("Unknown action: {}: {}".format(action, action_input))
             print(" -- running {} {}".format(action, action_input))
-            observation = known_actions[action](action_input)
+            if action == 'sql_query':
+                action_input = ast.literal_eval(action_input)
+                observation = known_actions[action](*action_input)
+            else:
+                observation = known_actions[action](action_input)
             print("Observation:", observation)
             next_prompt = "Observation: {}".format(observation)
         else:
+
             return
 
 
@@ -133,5 +190,7 @@ question1 = """I have 2 dogs, a border collie and a scottish terrier. \
 What is their combined weight"""
 question2 = """I have 3 items, 2 pencils and a notebook. \
 What is their combined cost?"""
+question3 = """Create a scenario where you increase prices for pepsi brand"""
 print(f"Question: {question2}")
 query(question2)
+
